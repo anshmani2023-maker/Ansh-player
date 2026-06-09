eba228564aebabc6be2e9bcb3ab63a849962626c
anshmani2023-maker <anshmani2023@gmail.com>
2026-06-09 12:33:00 +0530

    Ansh Player upgraded

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let songs = [
     "song2.mp3",
     "song3.mp3"
 ];
+
 let songNames = [
     "Kesariya",
     "Tum Hi Ho",
-    "Galliyan returns"
+    "Galliyan Returns"
 ];
 
 let currentSong = 0;
@@ -14,53 +15,81 @@ let currentSong = 0;
 let song = document.getElementById("song");
 let progress = document.getElementById("progress");
 let title = document.getElementById("songTitle");
+let currentTimeText = document.getElementById("currentTime");
+let durationText = document.getElementById("duration");
+let volume = document.getElementById("volume");
+let bottomTitle = document.getElementById("bottomTitle");
 
 song.src = songs[currentSong];
+title.innerText = songNames[currentSong];
+bottomTitle.innerText = songNames[currentSong];
 
-function playSong(){
+function playSong() {
     song.play();
 }
 
-function pauseSong(){
+function pauseSong() {
     song.pause();
 }
 
-function nextSong(){
+function updateSong() {
+    song.src = songs[currentSong];
+    title.innerText = songNames[currentSong];
+    bottomTitle.innerText = songNames[currentSong];
+    song.play();
+}
 
+function nextSong() {
     currentSong++;
 
-    if(currentSong >= songs.length){
+    if (currentSong >= songs.length) {
         currentSong = 0;
     }
 
-    song.src = songs[currentSong];
-    title.innerText = songNames[currentSong];
-    song.play();
+    updateSong();
 }
 
-function prevSong(){
-
+function prevSong() {
     currentSong--;
 
-    if(currentSong < 0){
+    if (currentSong < 0) {
         currentSong = songs.length - 1;
     }
 
-    song.src = songs[currentSong];
-    title.innerText = songNames[currentSong];
-    song.play();
+    updateSong();
 }
+
+function playSpecificSong(index) {
+    currentSong = index;
+    updateSong();
+}
+
+song.addEventListener("ended", () => {
+    nextSong();
+});
 
 song.addEventListener("timeupdate", () => {
 
-    progress.max = song.duration;
-
+    progress.max = song.duration || 0;
     progress.value = song.currentTime;
 
+    let currentMin = Math.floor(song.currentTime / 60);
+    let currentSec = Math.floor(song.currentTime % 60);
+
+    let totalMin = Math.floor((song.duration || 0) / 60);
+    let totalSec = Math.floor((song.duration || 0) % 60);
+
+    if (currentSec < 10) currentSec = "0" + currentSec;
+    if (totalSec < 10) totalSec = "0" + totalSec;
+
+    currentTimeText.innerText = currentMin + ":" + currentSec;
+    durationText.innerText = totalMin + ":" + totalSec;
 });
 
 progress.addEventListener("input", () => {
-
     song.currentTime = progress.value;
+});
 
+volume.addEventListener("input", () => {
+    song.volume = volume.value / 100;
 });
